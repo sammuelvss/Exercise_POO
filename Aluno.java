41a60e1e7c1f65306564a1f5c012538a154e31da
Sammuel Victor <samvss13@gmail.com>
2026-04-10 20:13:03 +0000

    class Aluno

---
+++ Aluno.java
@@ -0,0 +1,34 @@
+public class Aluno extends Usuario {
+
+    public Aluno(int codigo, String nome, String email) {
+        super(codigo, nome, email);
+    }
+
+    @Override
+    public int getLimiteEmprestimos() {
+        return 3;
+    }
+
+    @Override
+    public int getPrazoDevolução() {
+
+        return 7;
+    }
+
+    @Override
+    public double getValorMultaDiaria() {
+
+        return 2.50;
+    }
+
+    @Override
+    public String getTipoUsuario() {
+        return "Aluno";
+    }
+
+    @Override
+    public String exibirResumo() {
+        return "(ALUNO) Código: " + getCodigo() + " | Nome: " + getNome() + " | Email: " + getEmail();
+
+    }
+}
